465a665c607e9a20a99db20e28e30225d0353e02
RIGO FEDERICO <19955@EDU.MARCONI>
2025-01-20 11:06:13 +0100

    base app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+def see_all_contacts():
+    ...
+
+def add_a_contact():
+    ...
+
+def delete_a_contact():
+    ...
+
+def search_for_a_contact():
+    ...
+
+def start_routine():
+    print('''premere :
+          1 per vedere tutti i contatti
+          2 per aggiungere un contatto
+          3 per eliminare un contatto
+          4 per cercare un contatto''')
+    
+    n = int(input())
+
+    if (n==1):
+        see_all_contacts()
+    elif(n==2):
+        add_a_contact() 
+    elif (n==3):
+        delete_a_contact()  
+    elif (n==4):
+        search_for_a_contact()
+    else:
+        print('inserisci un numero valido')
+        start_routine()
+
+if __name__ == "__main__":
+    start_routine()
+
