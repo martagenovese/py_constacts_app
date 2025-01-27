dd736c0e49034fad60504cfc435ae190c1ff4994
MARAULA ALESSIO <19889@EDU.MARCONI>
2025-01-27 10:29:07 +0100

    base utils

---
+++ utils.py
@@ -0,0 +1,15 @@
+import sqlite3
+connection = sqlite3.connect('data.db')
+
+
+def see_all_contacts():
+    ...
+
+def add_a_contact():
+    ...
+
+def delete_a_contact():
+    ...
+
+def search_for_a_contact():
+    ...
