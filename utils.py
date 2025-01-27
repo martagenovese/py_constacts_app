29e5d0e4dacc392ce8dc49a18847d03520fd5cf0
GENOVESE MARTA <19841@EDU.MARCONI>
2025-01-27 10:40:37 +0100

    feature add a contact added

--- utils.py
+++ utils.py
@@ -5,9 +5,11 @@ connection = sqlite3.connect('data.db')
 def see_all_contacts():
     ...
 
-def add_a_contact():
-    ...
-
+def add_a_contact(nome, cognome, telefono):
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO contatti (nome, cognome, telefono) VALUES (?, ?, ?)", (nome, cognome, telefono))
+    connection.commit()
+    
 def delete_a_contact():
     ...
 
