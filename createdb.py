20a80d0b96e005c3885289ca502a6377648aa95c
MARAULA ALESSIO <19889@EDU.MARCONI>
2025-01-27 10:22:29 +0100

    database create

---
+++ createdb.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+#
+# Establishing connection
+#
+
+# open a SQLite connection
+# a database file called data.db will be created,
+# if it does not exist
+connection = sqlite3.connect('data.db')
+
+# create a database cursor
+cur = connection.cursor()
+
+#
+# Creating the table
+#
+
+# create the database table if it doesn't exist
+table_schema = """
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(30) NOT NULL,
+    cognome varchar(30) NOT NULL,
+    telefono char(10) NOT NULL UNIQUE
+);
+"""
+cur.execute(table_schema)
+
+#
+# Cleaning up
+#
+
+# close the cursor
+cur.close()
+
+# close the connection
+connection.close()
