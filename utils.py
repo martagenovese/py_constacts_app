f15674d72f372ce4a224447c74b1a0ad1c94a705
RIGO FEDERICO <19955@EDU.MARCONI>
2025-01-27 10:47:50 +0100

    crerat

--- utils.py
+++ utils.py
@@ -8,8 +8,19 @@ def see_all_contacts():
 def add_a_contact():
     ...
 
-def delete_a_contact():
-    ...
+def delete_a_contact(id):
+    # create a database cursor
+    cur = connection.cursor()
+
+    # query the database for ALL data in the notes table
+    cur.execute('DELETE FROM contacts WHERE id = '+id+';')
+
+    # print the result
+    result = cur.fetchall()
+    print(result)
+
+    # close the cursor
+    cur.close()
 
 def search_for_a_contact():
     ...
